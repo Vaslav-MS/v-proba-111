609c0737a3113e3806263386c403a1231289ca79
Vaslav <2key@rambler.ru>
2024-10-21 12:59:49 +0500

    formula

--- test3.py
+++ test3.py
@@ -1,4 +1,4 @@
-simba = 'g', 'yu', 'fa', 45, 'god', 457983566, 'dasagfhfg dsfg', 234, 0
+simba = 'g', 'yu', 'fa', 45, 'god', 457983566, 'dasagfhfg dsfg', 234, 0, 111, 100, 5, 1000, 452385, 8
 print(simba)
 
 for i in simba:
